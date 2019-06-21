2bbdfe6764685b35232e4d66431b47450bcd0981
Rob <virtual-monk@users.noreply.github.com>
2019-06-21 16:16:05 -0500

    Update php@7.2.15.rb

--- Formula/php@7.2.15.rb
+++ Formula/php@7.2.15.rb
@@ -1,6 +1,6 @@
 class PhpAT7215 < Formula
   desc "General-purpose scripting language"
-  homepage "https://secure.php.net/"
+  homepage "https://www.php.net/"
   url "https://www.php.net/distributions/php-7.2.15.tar.xz"
   sha256 "75e90012faef700dffb29311f3d24fa25f1a5e0f70254a9b8d5c794e25e938ce"
 
